eed902174da34735e07f3e3ab16ea336afd2138a
TheMeinerLP <p.glanz@madfix.me>
2024-03-23 19:49:59 +0100

    Bump run task gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    id("xyz.jpenilla.run-paper") version "2.2.2"
+    id("xyz.jpenilla.run-paper") version "2.2.3"
     java
     id("org.jetbrains.kotlin.jvm") version "1.9.22"
 }
