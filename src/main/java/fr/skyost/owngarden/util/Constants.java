889ce08cab03dc567df04ba60c92cf2261a53b3c
Romindous <--global>
2024-04-05 19:25:54 -0400

    Moved Constants to a separate class

---
+++ src/main/java/fr/skyost/owngarden/util/Constants.java
@@ -0,0 +1,10 @@
+package fr.skyost.owngarden.util;
+
+public final class Constants {
+
+    private Constants() throws IllegalAccessException {
+        throw new IllegalAccessException("");
+    }
+
+    public static final String TREES = "trees";
+}
